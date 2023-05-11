3598a52b700bec7d9d09f7e44822d9de45bd4de4
JRMartinez <30609352+FrankMorro@users.noreply.github.com>
2023-05-11 09:31:20 -0400

    Add the click event to all buttons

--- script.js
+++ script.js
@@ -1,2 +1,47 @@
+//  Display Section
+const previousOperandDisplay = document.querySelector('[data-previous-operand]')
+const currentOperandDisplay = document.querySelector('[data-current-operand]')
+
+// Clear and backspace section
+const percentageButton = document.querySelector(['data-percentage'])
+const clearButton = document.querySelector(['data-clear'])
+const allClearButton = document.querySelector(['data-all-clear'])
+const backspaceButton = document.querySelector(['data-backspace'])
+
+// Math Function section
+const frac1xButton = document.querySelector(['data-frac1x'])
+const squareButton = document.querySelector(['data-square'])
+const daricButton = document.querySelector(['data-daric'])
+
+// Digit, operation and equals section
 const digitButtons = document.querySelectorAll(['data-digit'])
 const operationButtons = document.querySelectorAll(['data-operation'])
+const equalsButton = document.querySelector(['data-equals'])
+
+// Event Listeners
+// Percentage Button
+percentageButton.addEventListener('click', () => {})
+// Clear Button
+clearButton.addEventListener('click', () => {})
+// All Clear Button
+allClearButton.addEventListener('click', () => {})
+// Backspace Button
+backspaceButton.addEventListener('click', () => {})
+
+// Frac1x Button
+frac1xButton.addEventListener('click', () => {})
+//Square Button
+squareButton.addEventListener('click', () => {})
+// Daric Button
+daricButton.addEventListener('click', () => {})
+
+// Equals Button
+// Digit Buttons
+digitButtons.forEach((button) => {
+  button.addEventListener('click', () => {})
+})
+// Operation Buttons
+operationButtons.forEach((button) => {
+  button.addEventListener('click', () => {})
+})
+equalsButton.addEventListener('click', () => {})
